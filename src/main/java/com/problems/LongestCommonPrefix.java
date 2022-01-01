bd49ac7f93d974a04d70594f59dfd30afe0cccad
Jawad Siddiqui <jsiddiqui@wsgc.com>
2022-01-01 11:45:58 +0500

    Problem solving

---
+++ src/main/java/com/problems/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+package com.problems;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] str = {"flower", "flow", "flight"};
+        String prefix = longestCommonPrefix(str);
+        System.out.println(prefix);
+    }
+    public static String longestCommonPrefix(String[] strs) {
+        //it will iterate through
+        if(strs.length==0) return  "";
+        String prefix = strs[0];
+        for(int i=1; i<strs.length; i++)
+        {
+            while (strs[i].indexOf(prefix)!=0){
+                prefix = prefix.substring(0, prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
---
+++ src/main/java/com/problems/StringNoRepeat.java
@@ -0,0 +1,32 @@
+package com.problems;
+
+import java.util.HashSet;
+
+public class StringNoRepeat {
+    public static void main(String[] args) {
+        String s = "abbaaababa";
+        System.out.println(lengthOfLongestSubstring(s));
+
+    }
+    public static  int lengthOfLongestSubstring(String s)
+    {
+        HashSet<Character> hashSet = new HashSet<>();
+        int a_pointer = 0;
+        int b_pointer = 0;
+        int max = 0;
+        while(b_pointer < s.length())
+        {
+            if(!hashSet.contains(s.charAt(b_pointer)))
+            {
+                hashSet.add(s.charAt(b_pointer));
+                b_pointer++;
+                max = Math.max(hashSet.size(), max);
+            } else {
+                hashSet.remove(s.charAt(a_pointer));
+                a_pointer++;
+            }
+        }
+        return max;
+    }
+
+}
