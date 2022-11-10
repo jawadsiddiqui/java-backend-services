0c8eadb85837e3b2d860e08fd460f4f55472eb28
Jawad Siddiqui <68392308+jawadsiddiqui@users.noreply.github.com>
2022-11-10 20:32:57 +0500

    Consumer Interface (#3)
    Co-authored-by: Jawad Siddiqui <jsiddiqui@wsgc.com>

---
+++ src/main/java/com/designpatterns/DesignPatternExamples.java
@@ -0,0 +1,7 @@
+package com.designpatterns;
+
+public class DesignPatternExamples {
+    public static void main(String[] args) {
+        System.out.println("This is cusotm design patterns");
+    }
+}
