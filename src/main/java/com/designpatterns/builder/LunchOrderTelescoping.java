569b5e649de241e02ca8bfde44b3651022c11e17
Jawad Siddiqui <68392308+jawadsiddiqui@users.noreply.github.com>
2022-11-15 21:14:34 +0500

    Singleton Design Pattern in Java : Creational (#8)
    Co-authored-by: Jawad Siddiqui <jsiddiqui@wsgc.com>

---
+++ src/main/java/com/designpatterns/builder/LunchOrderTelescoping.java
@@ -0,0 +1,20 @@
+package com.designpatterns.builder;
+
+public class LunchOrderTelescoping {
+    private String bread;
+    private String condiments;
+    private String dressing;
+    private String meat;
+
+    public LunchOrderTelescoping(String bread)
+    {
+        this.bread = bread;
+    }
+
+    public LunchOrderTelescoping(String bread, String condiments)
+    {
+        //telecoping builder exampld
+        this(bread);
+        this.condiments = condiments;
+    }
+}
