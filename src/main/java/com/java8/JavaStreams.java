c1d1fcbc979e0a361ede565c37e72bd83f354ec3
Jawad Siddiqui <68392308+jawadsiddiqui@users.noreply.github.com>
2022-12-14 20:55:00 +0500

    JPMorgan: Frequency of Char in String (#25)
    * Two Sum Problem
    
    * Stream using flatmap
    
    * JPMorgan Question : Frequency of character in string
    
    Co-authored-by: Jawad Siddiqui <jsiddiqui@wsgc.com>

---
+++ src/main/java/com/java8/JavaStreams.java
@@ -0,0 +1,7 @@
+package com.java8;
+
+public class JavaStreams {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/leetcode/studyplan/datastructure/DataStructureStudy.java
@@ -0,0 +1,36 @@
+package com.leetcode.studyplan.datastructure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataStructureStudy {
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1,2,3,1}));
+    }
+
+    //bruteforce strategy
+    public static boolean containsDuplicate(int[] nums) {
+        for(int i=0; i<nums.length; i++){
+            for(int y=i+1; y<nums.length; y++){
+                if(nums[i]==nums[y]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean containDuplicateUsingMap(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return true;
+            } else
+            {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
