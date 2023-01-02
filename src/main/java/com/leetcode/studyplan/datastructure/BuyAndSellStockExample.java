6823bc07d3e11780a23ddda6cdef2b32cd98fa80
Jawad Siddiqui <jsiddiqui@wsgc.com>
2023-01-02 23:55:48 +0500

    Intersection of Array , Buy And SellStock Example

---
+++ src/main/java/com/leetcode/studyplan/datastructure/BuyAndSellStockExample.java
@@ -0,0 +1,14 @@
+package com.leetcode.studyplan.datastructure;
+
+public class BuyAndSellStockExample {
+    public static void main(String[] args) {
+        int[] arr = new int[]{7,4,3,2,1};
+        int i = maxProfit(arr);
+        System.out.println(i);
+    }
+
+    public static int maxProfit(int[] prices) {
+        
+        return 0;
+    }
+}
